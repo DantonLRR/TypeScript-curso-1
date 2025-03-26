a9706148ead3a124f0d67dd9b25d83341c1b1009
Danton Lucas <danyonlucasrr@gmail.com>
2025-03-26 17:44:15 -0300

    configuraçao do ambiente e primeiros passos

---
+++ app/app.ts
@@ -0,0 +1,3 @@
+import {Negociacao } from './models/neogociacao.js'
+
+const negociacao = new Negociacao( new Date(),10,100)
---
+++ dist/js/app.js
@@ -0,0 +1,2 @@
+import { Negociacao } from './models/neogociacao.js';
+const negociacao = new Negociacao(new Date(), 10, 100);
